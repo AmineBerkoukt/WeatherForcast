a8022b3d31cd7827a2fecc0b7b11e2ca71f32008
Amine Berkoukt <amine.berkoukt@gmail.com>
2024-12-26 17:08:05 +0100

    All scrappers done !

--- server/src/scrappers/rainProbabilityScrapper.js
+++ server/src/scrappers/rainProbabilityScrapper.js
@@ -7,17 +7,35 @@ config();
 baseURL = process.env.BASE_URL || "https://www.meteomaroc.com/meteo/tanger";
 
 // Need to extract the average humidity today & the next 7 days
+// target class : precipmm
 const rainProbabilityData = async () => {
-    try {  
-        const response = await requestPromise(baseURL); 
-
-        const $ = cheerio.load(response);  
+    try {
+        const response = await requestPromise(baseURL);
+        const $ = cheerio.load(response);
         console.log("cheerio loaded !");
 
-       
-        console.log("Scrap test : " , response);
 
-    }catch (error) {
+
+        const days = [1, 2, 3, 4, 5, 6, 7];
+        const rainProbabilities = {};
+
+        days.forEach(day => {
+            const tempProbability = $(`ul.previsiondecjour-total-bottomaccueil > a[href="/previsions/tanger#jour-${day}"] > li.precipmm`)
+                .text().trim();
+            if (day === 1) {
+                rainProbabilities[`Today`] = tempProbability;
+            } else if (day === 2) {
+                rainProbabilities[`Tomorrow`] = tempProbability;
+            } else {
+                rainProbabilities[`TodayPlus${day - 1}`] = tempProbability;
+            }
+        });
+
+        console.log(rainProbabilities)
+
+        res.status(200).json(rainProbabilities);
+
+    } catch (error) {
         console.error('(scrappers/rainProbabilityData.js) : Error occurred during scraping:', error);
     }
 };
--- server/src/scrappers/testScrapper.js
+++ server/src/scrappers/testScrapper.js
@@ -13,45 +13,26 @@ const testScrapper = async (req, res) => {
         const $ = cheerio.load(response);
         console.log("cheerio loaded !");
 
-        const days = [2, 3, 4, 5, 6, 7];
-        const labels = ['today', 'tomorrow', 'todayPlus2', 'todayPlus3', 'todayPlus4', 'todayPlus5'];
-        const weatherStates = {};
+
+
+        const days = [1, 2, 3, 4, 5, 6, 7];
+        const rainProbabilities = {};
 
         days.forEach(day => {
-            let tempWeatherState = "";
-            let tempPrecipitation = $(`a[href="/previsions/tanger#jour-${day}"] > label[class^="weathericon-small-small-heur-jour meddelt"]`).attr('class').trim();
-
-            switch (tempPrecipitation) {
-                case "weathericon-small-small-heur-jour meddelt-113":
-                    tempWeatherState = "Sunny"
-                    break;
-
-                case "weathericon-small-small-heur-jour meddelt-116":
-                    tempWeatherState = "Sunny & Cloudy"
-                    break;
-
-                case "weathericon-small-small-heur-jour meddelt-119":
-                    tempWeatherState = "Extremely Cloudy"
-                    break;
-
-                case "weathericon-small-small-heur-jour meddelt-266":
-                    tempWeatherState = "Rainy"
-                    break;
-
-                case "weathericon-small-small-heur-jour meddelt-389":
-                    tempWeatherState = "Heavily Rainy"
-                    break;
-
-                default:
-                    tempWeatherState = "Not available";
-                    break;
+            const tempProbability = $(`ul.previsiondecjour-total-bottomaccueil > a[href="/previsions/tanger#jour-${day}"] > li.precipmm`)
+                .text().trim();
+            if (day === 1) {
+                rainProbabilities[`Today`] = tempProbability;
+            } else if (day === 2) {
+                rainProbabilities[`Tomorrow`] = tempProbability;
+            } else {
+                rainProbabilities[`TodayPlus${day - 1}`] = tempProbability;
             }
+        });
 
-            weatherStates[`TodayPlus${day - 1}`] = tempWeatherState;
-        })
+        console.log(rainProbabilities)
 
-
-        res.status(200).json(weatherStates);
+        res.status(200).json(rainProbabilities);
 
     } catch (error) {
         console.error('(scrappers/testScrapper.js) : Error occurred during scraping:', error.message);
